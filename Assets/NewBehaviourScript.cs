5bb93eeab296e3400be82ca2764ce3bfa6603fe8
neyctech <gzh.ding@outlook.com>
2018-06-03 11:08:23 +0800

    push # whatever
    trying to push :)

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(Upload());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator Upload()
+    {
+        List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
+        formData.Add(new MultipartFormDataSection("apikey = rwTOUyXt226v50MPFDU1tAQrAFQrvESAUu8QwS4UAjpc"));
+        formData.Add(new MultipartFormFileSection("images_file", "/var/mobile/Containers/Data/app sandbox/Documents/screenshot.jpg"));
+        UnityWebRequest www = UnityWebRequest.Post("https://gateway.watsonplatform.net/visual-recognition/api", formData);
+        yield return www.SendWebRequest();
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            Debug.Log("Form upload complete!");
+        }
+    }
+}
